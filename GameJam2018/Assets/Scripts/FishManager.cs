7d85eedba2eb2fade0bc3d6f5385a3cbfb4c90ad
Max Bryans <maxsfw3@gmail.com>
2018-11-09 20:08:34 +0000

    We have Fish

--- GameJam2018/Assets/Scripts/FishManager.cs
+++ GameJam2018/Assets/Scripts/FishManager.cs
@@ -5,14 +5,39 @@ using UnityEngine;
 public class FishManager : MonoBehaviour {
 
     public Transform fish;
+    public float height = 50;
+    public float Radius = 50;
+    public int FishCount = 20;
+    public FishScript[] Pool;
+    private Vector3 _target;
+
+    [Header("Fish Variables")]
+    [Range(0, 20)]
+    public float Speed = 3;
+    [Range(0, 90)]
+    public float TurnRate = 90;
+    [Range(0, 20)]
+    public float Vision = 20;
 
 
-	// Use this for initialization
-	void Start () {
-		
-       
+    // Use this for initialization
+    void Start () {
 
-	}
+        Pool = new FishScript[FishCount];
+        for(int i = 0; i < FishCount; i++)
+        {
+            float SpawnHeight = Random.Range(1f, height - 1f);
+            float SpawnAngle = Random.Range(0f, 2*Mathf.PI);
+            Transform MyFish = Instantiate(fish, new Vector3(Mathf.Cos(SpawnAngle) * Random.Range(0,Radius), SpawnHeight, Mathf.Sin(SpawnAngle) * Random.Range(0, Radius)),Quaternion.identity);
+            Pool[i] = MyFish.GetComponent<FishScript>();
+            Pool[i].ID = i;
+        }
+        for (int i = 0; i < FishCount; i++)
+        {
+            Pool[i].Others = Pool;
+        }
+
+        }
 	
 	// Update is called once per frame
 	void Update () {
--- GameJam2018/Assets/Scripts/FishScript.cs
+++ GameJam2018/Assets/Scripts/FishScript.cs
@@ -6,8 +6,12 @@ public class FishScript : MonoBehaviour {
 
     FishManager FM;
     public Vector3 Velocity;
+    public Vector3 Position;
     public Vector3 Speed;
     public Vector3 MaxSpeed;
+    public Vector3 CoM;
+    public int ID;
+    public FishScript[] Others;
 
 
 
@@ -15,11 +19,46 @@ public class FishScript : MonoBehaviour {
 	void Start () {
         // find Manager
         FM = FindObjectOfType<FishManager>();
+        Position = GetComponentInParent<Transform>().position;
 
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        CoM = Vector3.zero;
+        int CoMCount = 0;
+        Vector3 CoR = Vector3.zero;
+        int CoRCount = 0;
+        //double start = Time.realtimeSinceStartup;
+        foreach (FishScript FS in Others)
+        {
+
+            if (FS.ID != ID) // don't compare on self
+            {
+                if (Vector3.Distance(transform.position,FS.transform.position) < FM.Vision)
+                {
+                    CoM += (FS.transform.position - transform.position);
+                    CoMCount++;
+                    CoR += (FS.Velocity - Velocity);
+                    CoRCount++;
+                }
+            }
+            if (ID == 4) Debug.Log(CoMCount + " found");
+            if (CoMCount > 0) CoM /= (float)CoMCount;
+            if (CoRCount > 0) CoR /= (float)CoRCount;
+        }
+        //Debug.Log("Time to Search " + (Time.realtimeSinceStartup - start));
+
+
+        //Quaternion target = Quaternion.Euler(CoR.x, CoR.y, CoR.z);
+        // Dampen towards the target rotation
+        //transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * FM.TurnRate);
+        //transform.Rotate(transform.up, FM.TurnRate * Time.deltaTime);
+
+        transform.rotation = Quaternion.LookRotation(CoM, Vector3.up);
+        transform.Translate(transform.forward * FM.Speed * Time.deltaTime,Space.World);
+     
+
 	}
 }
